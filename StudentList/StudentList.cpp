d3339a64d8c862fc952290daa31c207ad8d594a9
U-BSD\394621 <394621@SHS-DGDPCS2.beaverton.k12.or.us>
2022-10-11 11:51:13 -0700

    Finished project. Added the deleteFunction method and cleaned up user interface.

--- StudentList/StudentList.cpp
+++ StudentList/StudentList.cpp
@@ -4,16 +4,19 @@ Student List Program
 C++ Programming
 Period 5
 Mr. Galbraith
-Project Completed: 
-Outside Resources Utilized:
+Project Completed: 10/11/2022
+Outside Resources Utilized: Created in collaboration with Daren Kostov. Used https://www.geeksforgeeks.org/how-does-a-vector-work-in-c/ to help me understand vectors. Used https://www.youtube.com/watch?v=2ybLD6_2gKM to help me understand pointers.
+
+This program takes 4 different commands which allow a user to manipulate a student directory system. The program can store the students' names, ID number, and gpa.
  */
 
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-struct Student {
+struct Student {//initializes the struct of Student objects
   char name[64];
   char lastName[64];
   int id;
@@ -29,7 +32,7 @@ struct Student {
 //initialize functions
 void printFunction(vector <Student> StudentList);
 void addFunction(vector <Student> *StudentListPtr);
-void deleteFunction();
+void deleteFunction(vector <Student> *StudentListPtr);
 
 int main() {
   vector <Student> StudentList;//initializes a vector (a dynamic array) of Students (which are the struct)
@@ -41,7 +44,7 @@ int main() {
   //char random[64] = "Nathan";
   //strcpy(StudentList[0].name, random);
   
-  while(true) {
+  while(true) {//will continually ask for new command inputs
     char input[10];
     cin >> input;
     
@@ -49,13 +52,13 @@ int main() {
 	input[1] == 'R' &&
 	input[2] == 'I' &&
 	input[3] == 'N' &&
-	input[4] == 'T') {
+	input[4] == 'T') {//if the user types in "PRINT," the printFunction method will be run
       printFunction(StudentList);
     }
 
     if (input[0] == 'A' &&
 	input[1] == 'D' &&
-	input[2] == 'D') {
+	input[2] == 'D') {//if the user types in "ADD," the addFunction method will be run
       addFunction(StudentListPtr);
     }
 
@@ -64,14 +67,14 @@ int main() {
 	input[2] == 'L' &&
 	input[3] == 'E' &&
 	input[4] == 'T' &&
-	input[5] == 'E') {
-      //deleteFunction();
+	input[5] == 'E') {//if the user types in "DELETE," the deleteFunction method will be run
+      deleteFunction(StudentListPtr);
     }
 
     if (input[0] == 'Q' &&
 	input[1] == 'U' &&
 	input[2] == 'I' &&
-	input[3] == 'T') {
+	input[3] == 'T') {//if the user types in "QUIT," the program will stop running
       exit(0);
     }
     cout << endl;
@@ -79,14 +82,14 @@ int main() {
   }
 }
 
-void printFunction(vector <Student> StudentList) {//Next step: Daren said: "Put the pointer in the add function."
+void printFunction(vector <Student> StudentList) {//prints out each student in the vector and their corresponding information
   vector<Student>::iterator ptr;
   for (ptr = StudentList.begin(); ptr < StudentList.end(); ptr++) {
-    cout << "Name: " << ptr -> name << " " << ptr -> lastName << ";   ID#: " << ptr -> id << ";   GPA: " << ptr -> GPA << endl;
+    cout << "Name: " << ptr -> name << " " << ptr -> lastName << ";   ID#: " << ptr -> id << ";   GPA: " << floor(ptr -> GPA*100)/100 << endl;
   }
 }
 
-void addFunction(vector <Student> *StudentListPtr) {
+void addFunction(vector <Student> *StudentListPtr) {//adds a new student into the vector
   char firstName[64];
   char lastName[64];
   int idNum;
@@ -99,7 +102,22 @@ void addFunction(vector <Student> *StudentListPtr) {
   cin >> idNum;
   cout << "Please enter the student's grade point average." << endl;
   cin >> gpa;
-  StudentListPtr -> push_back(Student(firstName, lastName, idNum, gpa));//adding a empty student struct to the vector
+  StudentListPtr -> push_back(Student(firstName, lastName, idNum, gpa));//adding a student into the vector will all the associated information
   cout << endl;
   cout << firstName << " " << lastName << " added." << endl;
+}
+
+void deleteFunction(vector <Student> *StudentListPtr) {//goes through the vector of students, trying to match an inputted ID number. If the ID number inputted matches an ID number found in the vector, that student is deleted from the vector.
+  cout << "ID: ";
+  int target;
+  cin >> target;
+  vector<Student>::iterator itr;
+  for (itr = StudentListPtr -> begin(); itr < StudentListPtr -> end(); itr++) {
+    if (itr -> id == target) {
+      cout << itr -> name << " " << itr -> lastName << " has been deleted from the system." << endl;
+      StudentListPtr -> erase(itr);
+      return;
+    }
+  }
+  cout << "This student does not exist." << endl; //if the inputted ID number can't be found
 }
