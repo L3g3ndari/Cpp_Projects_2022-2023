ce30ce2065b8db9b8428715e86eb71e24189360d
U-BSD\394621 <394621@SHS-DGDPCS2.beaverton.k12.or.us>
2022-11-29 11:47:09 -0800

    Finished project. Completed the music class and integrated into the main class. Worked out a few bugs.

--- MediaDatabase/main.cpp
+++ MediaDatabase/main.cpp
@@ -1,11 +1,21 @@
-//https://cplusplus.com/reference/cstring/strstr/
+/*
+Media Database
+Nathan Wu
+C++ Programming
+Mr. Galbraith
+Period 5
+Projected Completed: 11/29/22
+Sources used: https://cplusplus.com/reference/cstring/strstr for learning how to use the strstr function
+
+This program stores different types of media and their respective qualities which are entered by the user.
+*/
 
 #include <iostream>
 #include <vector>
 #include <cstring>
 #include "vg.h"
 #include "movie.h"
-//#include "music.h"
+#include "music.h"
 
 using namespace std;
 
@@ -16,24 +26,23 @@ void printFunction(vector <Media*> Database);
 bool cmpStr(char* str1, char* str2);
 
 int main() {
-  vector <Media*> Database;
-  //vector <Media> *DatabasePtr = &Database;
+  vector <Media*> Database;//a vector of pointers must be used in this instance because the children can't be added directly to a vector of Media (the parent class). So we use pointers to allow us to consolidate the media being stored to one vector.
   cout << "Welcome to your Media Database." << endl;
   cout << "Available commands: ADD, SEARCH, DELETE, QUIT." << endl;
     
-  while (true) {
+  while (true) {//asks for new commands and executes them when inputted
     char input[10];
     cout << "Awaiting new command: ";
     cin >> input;
-
+    //should have used strcmp smh but this works too
     if (input[0] == 'A' &&
 	input[1] == 'D' &&
 	input[2] == 'D') {
-      addFunction(Database);
+      addFunction(Database);//adds a media to the database
     }
 
     if(input[0] == 'P'){
-      printFunction(Database);
+      printFunction(Database);//prints all the media in the database
     }
 
     if (input[0] == 'S' &&
@@ -42,7 +51,7 @@ int main() {
 	input[3] == 'R' &&
 	input[4] == 'C' &&
 	input[5] == 'H') {
-      searchFunction(Database);
+      searchFunction(Database);//accepts a search term and displays all the media in the database that match the search term. Can search by title or year.
     }
 
     if (input[0] == 'D' &&
@@ -51,10 +60,10 @@ int main() {
 	input[3] == 'E' &&
 	input[4] == 'T' &&
 	input[5] == 'E') {
-      deleteFunction(Database);
+      deleteFunction(Database);//Uses the same functionality as the SEARCH function but systematically asks the user if they want to delete the search results.
     }
 
-    if (strcmp(input, "QUIT") == 0) {
+    if (strcmp(input, "QUIT") == 0) {//exits the program.
       cout << "Confirmation to quit the program. All your data will be lost. Do you want to quit?  (y/n)" << endl;
       char yesno[80] = "";
       cin >> yesno;
@@ -69,10 +78,11 @@ int main() {
   }
 }
 
-void addFunction(vector <Media*> &Database) {
+void addFunction(vector <Media*> &Database) {//adds a media to the database
   char title[80];
   int year;
   int typeInput;
+  //asks for all the qualities of the media being inputted. Some of these quatities change based on the type of media.
   cout << "Title: ";
   cin.clear();
   cin.ignore(80, '\n');
@@ -94,14 +104,33 @@ void addFunction(vector <Media*> &Database) {
     cin >> rating;
     cout << "Run time (in minutes): ";
     cin >> duration;
-    Database.push_back(new Movie(typeInput, title, year, rating, director, duration));
+    Database.push_back(new Movie(typeInput, title, year, rating, director, duration));//the movie is added to the database
     cout << endl;
     cin.clear();
     cin.ignore(80, '\n');
     cout << title << " has been added to your database." << endl;
   }
   if (typeInput == 2) {//music
-
+    char publisher[80];
+    char artist[80];
+    int duration;
+    cout << "Publisher: ";
+    cin.clear();
+    cin.ignore(80, '\n');
+    cin.get(publisher, 80);
+    cout << "Artist: ";
+    cin.clear();
+    cin.ignore(80, '\n');
+    cin.get(artist, 80);
+    cin.clear();
+    cin.ignore(80, '\n');
+    cout<< "Duration (in seconds): ";
+    cin >> duration;
+    Database.push_back(new Music(typeInput, title, year, publisher, artist, duration));//the music is added to the database
+    cout << endl;
+    cin.clear();
+    cin.ignore(80, '\n');
+    cout << title << " has been added to your database." << endl;
   }
   if (typeInput == 3) {//video games
     char publisher[80];
@@ -112,26 +141,26 @@ void addFunction(vector <Media*> &Database) {
     cin.get(publisher, 80);
     cout << "Rating (positive integer out of 10): ";
     cin >> rating;
-    Database.push_back(new VG(typeInput, title, year, rating, publisher));//can't add VG to a vector of Media, so need to create a vector of pointers to Media
+    Database.push_back(new VG(typeInput, title, year, rating, publisher));//the video game is added to the database
     cout << endl;
     cout << title << " has been added to your database." << endl;
   }
   
 }
 
-void printFunction(vector <Media*> Database) {
+void printFunction(vector <Media*> Database) {//runs through the vector of pointers and prints out everything.
   vector<Media*>::iterator i;
   for (i = Database.begin(); i < Database.end(); i++) {
     (*i) -> printInfo();
   }
 }
 
-void searchFunction(vector <Media*> Database) {
+void searchFunction(vector <Media*> Database) {//accepts a search term and displays all the media in the database that match the search term. Can search by title or year.
   char input[6] = "";
   cout << "Would you like to search by title or year?" << endl;
   cin >> input;
   cin.ignore(80, '\n');
-  if (strcmp(input, "title") == 0) {
+  if (strcmp(input, "title") == 0) {//uses the strstr function to check if the search term is contained in each entry
     char searchTerm[80] = "";
     cout << "Please type your search term (title): ";
     cin >> searchTerm;
@@ -146,7 +175,7 @@ void searchFunction(vector <Media*> Database) {
     }
     cout << endl;
   }
-  else if (strcmp(input, "year") == 0) {
+  else if (strcmp(input, "year") == 0) {//checks if the searched year is equal to any of the entries' year data
     int searchTerm = 0;
     cout << "Please type your search term (year): ";
     cin >> searchTerm;
@@ -165,7 +194,7 @@ void searchFunction(vector <Media*> Database) {
   }  
 }
 
-void deleteFunction(vector <Media*> &Database) {
+void deleteFunction(vector <Media*> &Database) {//Uses the same functionality as the SEARCH function but systematically asks the user if they want to delete the search results.
   vector <char*> toBeDeleted;
   char input[6] = "";
   cout << "Please search for the entries you want to delete. Would you like to search by title or year?" << endl;
@@ -188,13 +217,13 @@ void deleteFunction(vector <Media*> &Database) {
 	cout << "Would you like to delete this file from your media database? This action cannot be undone. (y/n)" << endl;
 	char yesno[80];
 	cin >> yesno;
-	//cout << "-" << endl;//somehow, this print line was crucial to the functionality of the program, even though it does nothing
+	//cout << "-" << endl;//somehow, this print line was crucial to the functionality of the program for a certain period of time, even though it does nothing
 	if (yesno[0] == 'y') {
 	  //cout << (*itr) -> getTitle() << endl;
 	  toBeDeleted.push_back((*itr) -> getTitle());
 	  //cout << "toBeDeleted1: " << *toBeDeleted.begin() << endl;
      	}
-	//cout << "-" << endl;//somehow, this print line was crucial to the functionality of the program
+	//cout << "-" << endl;//somehow, this print line was crucial to the functionality of the program for a certain period of time
       }
     }
     while (toBeDeleted.size() > 0) {
