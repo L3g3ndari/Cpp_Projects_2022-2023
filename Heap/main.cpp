52ba94108b370ba2b174027893141418639fb565
Nathan Wu <113073109+L3g3ndari@users.noreply.github.com>
2023-02-22 21:52:42 -0800

    Fixed a few bugs, started working on things but nothing major. Will work on more tmrw.

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -16,12 +16,12 @@ Description:
 using namespace std;
 
 void printFunction();
-void addFunction();
+void addFunction(int* heap[101]);
 void deleteFunction();
 
 int main() {
   cout << "Welcome to the Heap." << endl;
-  cout << "Available Commands: \"PRINT,\" \"ADD,\" \"DELETE,\" or \"QUIT\"" << endl;
+  cout << "Available Commands: \"PRINT (DISPLAY),\" \"ADD,\" \"DELETE,\" or \"QUIT\"" << endl;
 
   int* heap[101];
   for (int i = 0; i < 101; i++) {
@@ -34,12 +34,12 @@ int main() {
     cin.clear();
     cin.ignore(10, '\n');
 
-    if (strcmp(input, "PRINT") == 0) {//if the user types in "PRINT," the printFunction method will be run
+    if (strcmp(input, "PRINT") == 0 || strcmp(input, "DISPLAY") == 0) {//if the user types in "PRINT" or "DISPLAY the printFunction method will be run
       printFunction();
     }
 
     if (strcmp(input, "ADD") == 0) {//if the user types in "ADD," the addFunction method will be run
-      addFunction();
+      addFunction(heap);
     }
 
     if (strcmp(input, "DELETE") == 0) {//if the user types in "DELETE," the deleteFunction method will be run
@@ -55,20 +55,21 @@ int main() {
 }
 
 void printFunction() {
-
+  //start at slot index 1 and find its children, then move to number 2 and find its children, etc.
 }
 
-void addFunction(int* &heap[101]) {
+void addFunction(int* heap[101]) {
   int inputType = 0;
   int input;
   cout << "What is the type of input? Type \"1\" for console input or \"2\" for file input." << endl;
   cin >> inputType;
+  //clear and ignore
   if (inputType == 1) {//add as console input
     cout << "What number would you like to add?" << endl;
-    cin.getline(input, 64);
+    cin >> input;
     if (input > 0 && input <= 1000) {
       for (int i = 1; i < 101; i++) {//add to the array in the first available slot
-	if (head[i] == NULL) {
+	if (heap[i] == NULL) {
 	  //THIS IS WHERE YOU LEFT OFF ******************
 	}
       }
