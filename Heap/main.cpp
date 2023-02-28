a2455734766d94448dfa94ba7cc57c4d04547d44
Nathan Wu <113073109+L3g3ndari@users.noreply.github.com>
2023-02-27 22:46:42 -0800

    Added a lot to adding from file, but still couldn't get it to work properly. Added deleteFunction, but it's still buggy.

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 void printFunction(int heap[101]);
 int addFunction(int heap[101], int heapSize);
-void addByFile(int heap[101], char fileInputName[20]);
+void addByFile(int heap[101], char fileInputName[20], int heapSize);
 void deleteFunction(int heap[101]);
 void sortHeap(int heap[101]);
 
@@ -104,7 +104,7 @@ int addFunction(int heap[101], int heapSize) {
     char fileNameInput[20];
     cout << "What file do you want to access?" << endl;
     cin.getline(fileNameInput, 20);
-    cout << fileNameInput << endl;
+    //cout << fileNameInput << endl;
     cin.clear();
     //cin.ignore(20, '\n');
     cout << fileNameInput << endl;
@@ -112,8 +112,8 @@ int addFunction(int heap[101], int heapSize) {
 	strcmp(fileNameInput, "numFile2.txt") == 0 ||
 	strcmp(fileNameInput, "numFile3.txt") == 0 ||
 	strcmp(fileNameInput, "numFile4.txt") == 0) {
-      cout << "Hellooo" << endl;
-      addByFile(heap, fileNameInput);
+      //cout << "Hellooo" << endl;
+      addByFile(heap, fileNameInput, heapSize);
     }
     else {
       cout << "That file does not exist." << endl;
@@ -127,19 +127,37 @@ int addFunction(int heap[101], int heapSize) {
   return 101;
 }
 
-void addByFile(int heap[101], char fileNameInput[20]) {
+void addByFile(int heap[101], char fileNameInput[20], int heapSize) {
   cout << "Reading from \"" << fileNameInput << "\"" << endl;
   int temp[100];
+  for (int i = 0; i < 100; i++) {
+    temp[i] = 0;
+  }
   int num;
+  int count = 0;
+  int input;
   ifstream File (fileNameInput);
   if (File.is_open()) {
     while (File >> num) {
       cout << num << " ";
+      temp[count++] = num;
+    }
+    cout << endl;
+    for (int i = 0; i < 100; i++) {//ensures that the file translated to the temporary array correctly
+      cout << temp[i] << endl;
+      input = temp[i];
+      cout << input << endl;
+      for (int i = 1; i < 101; i++) {//add to the array in the first available slot
+        if (input > 0 && input <= 1000 && heapSize < 100 && heap[i] == 0) {
+          heap[i] = input;
+	}
+      }
     }
   }
   else {
     cout << "Unable to open first name file" << endl;
   }
+  cout << "Adding from file is complete" << endl;
 
   for (int i = 1; i < 101; i++) {//add to the array in the first available slot
     if (heap[i] == 0) {
@@ -153,7 +171,41 @@ void addByFile(int heap[101], char fileNameInput[20]) {
 }
 
 void deleteFunction(int heap[101]) {
-
+  cout << "Would you like to delete the root (\"r\"), or all of the heap (\"a\")?" << endl;
+  char ans[2];
+  cin >> ans;
+  int switchTargetIndex;
+  if (strcmp(ans, "r") == 0) {//delete root
+    for (int i = 2; i < 100; i++) {//find last element
+      if (heap[i] == 0) {
+	cout << "i = " << i << endl;
+	switchTargetIndex == i-1;//store the index of the one before the first 0;
+	break;
+      }
+    }
+    int temp = heap[switchTargetIndex];
+    heap[switchTargetIndex] = heap[1];//switch the root with the last slot
+    heap[1] = temp;//switch complete
+    cout << "Out: " << heap[switchTargetIndex] << endl;
+    heap[switchTargetIndex] = 0;//erase the original root
+    sortHeap(heap);
+  }
+  else if (strcmp(ans, "a") == 0) {//delete whole heap
+    cout << "Out: ";
+    for (int i = 1; i < 100; i++) {//traversing the heap
+      if (heap[i] != 0) {
+	cout << heap[i] << " ";
+	heap[i] = 999;
+      }
+      else {
+	return;
+      }
+    }
+    cout << endl << "Deleted whole heap." << endl;
+  }
+  else {
+    cout << "That was not a valid input." << endl;
+  }
 }
 
 void sortHeap(int heap[101]) {
@@ -166,14 +218,20 @@ void sortHeap(int heap[101]) {
       heap[i] = heap[i*2];
       heap[i*2] = temp;
       cout << "Performed swap with left child!" << endl;
-      sortHeap(heap);
+      sortHeap(heap);//call itself to loop it until completely sorted
     }
     while (heap[i] < heap[i*2+1]) {//check if parent is less than right child
       int temp = heap[i];
       heap[i] = heap[i*2+1];
       heap[i*2+1] = temp;
       cout << "Performed swap with right child!" << endl;
-      sortHeap(heap);
+      sortHeap(heap);//call itself to loop it until completely sorted
     }
   }
 }
+
+//STILL NEED TO FIX
+
+//Deleting from wrong spot, leaving a weird 0
+//Adding from file still doesn't work, won't grab individual numbers
+//Print as a tree
